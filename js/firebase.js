2fbbc72d7761e9fa365e63537c4e90470901286f
Ken-tech411 <71333435+Ken-tech411@users.noreply.github.com>
2021-05-15 19:11:47 +0700

    :))

---
+++ js/firebase.js
@@ -0,0 +1,9 @@
+var firebaseConfig = {
+    apiKey: "AIzaSyCOiz0MrvtBX4KgF8bS69uGz_E05nlPlkM",
+    authDomain: "wi01-quanlychitieu.firebaseapp.com",
+    projectId: "wi01-quanlychitieu",
+    storageBucket: "wi01-quanlychitieu.appspot.com",
+    messagingSenderId: "760798931236",
+    appId: "1:760798931236:web:341747b7bbce503136ebb1"
+};
+firebase.initializeApp(firebaseConfig);
