dafacefb1cd793b77a27de7430a3e2050398ad3d
Ken-tech411 <71333435+Ken-tech411@users.noreply.github.com>
2021-06-03 21:48:46 +0700

    xg nhan vat chinh

--- js/money.js
+++ js/money.js
@@ -42,3 +42,49 @@ document.addEventListener("DOMContentLoaded", async function (event) {
 function numberWithCommas(x) {
     return x.toString().replace(/\B(?<!\.\d*)(?=(\d{3})+(?!\d))/g, ",");
 };
+
+function renderTrans() {
+    auth.onAuthStateChanged(async function (user) {
+        console.log(user)
+        if (user) {
+            let wallet = document.getElementById("wallet");
+            let uid = user.uid;
+            let doc = await db.collection("users").doc(uid).get();
+            wallet.value = doc.data().name;
+
+        } else {
+            window.location.href = "/login.html";
+        }
+    })
+}
+renderTrans();
+
+let wallet = document.getElementById("wallet");
+let amount = document.getElementById("amount");
+let date = document.getElementById("date")
+let note = document.getElementById("note");
+let add_trans = document.getElementById("add_trans");
+
+var dnd = document.querySelector('#category');
+var logSelectedID = function () {
+    console.log(dnd.options[dnd.selectedIndex].id);
+};
+
+logSelectedID();
+dnd.addEventListener('change', logSelectedID);
+
+add_trans.addEventListener("click", async function () {
+    var dnd = document.querySelector('#category');
+    let selectedID = dnd.options[dnd.selectedIndex].id
+    let user = await auth.currentUser;
+    let email = await user.uid;
+    let category = db.collection("category").doc(selectedID)
+    db.collection("Transaction").add({
+        uid: email,
+        category: category,
+        money: parseInt(amount.value),
+        date: date.value,
+        note: note.value
+    })
+})
+
